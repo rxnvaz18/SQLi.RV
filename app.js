68d6a257295a8bbe428700ba90443eb85e48142b
unknown <roxanvaz31@gmail.com>
2024-03-10 22:21:02 -0700

    prepared for attack

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+// Dependencies
+
 const http = require('http'),
 path = require('path'),
 express = require('express'),
-bodyParser = require('body-parser');
-const sqlite3 = require('sqlite3').verbose();
+bodyParser = require('body-parser')
+
+const sqlite3 = require('sqlite3').verbose()
 const app = express();
 app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -10,8 +13,39 @@ app.use(bodyParser.json())
 
 // Creating the sqlite database
 
-const db = new sqlite3.Database(':memory:');
+const db = new sqlite3.Database(':memory:')
 db.serialize(function () {
- db.run("CREATE TABLE user (username TEXT, password TEXT, title TEXT)");
- db.run("INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')");
-});
+ db.run("CREATE TABLE user (username TEXT, password TEXT, title TEXT)")
+ db.run("INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')")
+})
+
+// GET method route
+app.get('/', function (req,res) {
+    res.sendFile('index.html')
+})
+
+// express POST route to handle forms that are submitted in html login form
+app.post('/login', function (req, res) {
+    let username = req.body.username
+    let password = req.body.password
+    let query = "SELECT title FROM user WHERE username = '" + username + "' AND password = '" + password + "';"
+
+    console.log("username: " + username)
+    console.log("password: " + password)
+    console.log('query: ' + query)
+
+    db.get(query, function (err, row) {
+
+		if (err) {
+			console.log('ERROR', err);
+			res.redirect("/index.html#error");
+		} else if (!row) {
+			res.redirect("/index.html#unauthorized");
+		} else {
+			res.send('Hello <b>' + row.title + '!</b><br /> This file contains all your secret data: <br /><br /> SECRETS <br /><br /> MORE SECRETS <br /><br /> <a href="/index.html">Go back to login</a>');
+		}
+	});
+    
+})
+
+app.listen(3000)
